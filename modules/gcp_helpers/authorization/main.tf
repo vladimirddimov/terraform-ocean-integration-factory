f4e1359586bfe79b276d7d0a50c83c6fd25a60e8
Vladimir Dimov <vladimir.dimov@checkatrade.com>
2024-05-30 09:23:50 +0300

    fix: comment out authz part

--- modules/gcp_helpers/authorization/main.tf
+++ modules/gcp_helpers/authorization/main.tf
@@ -4,28 +4,28 @@ resource "google_service_account" "ocean_integration_service_account" {
   display_name = "Ocean Integration Service Account"
 }
 
-resource "google_organization_iam_custom_role" "ocean_integration_iam_org_role" {
-  title       = "Ocean Integration organization role"
-  role_id     = var.role_id
-  permissions = var.permissions
-  org_id      = var.organization
-}
+# resource "google_organization_iam_custom_role" "ocean_integration_iam_org_role" {
+#   title       = "Ocean Integration organization role"
+#   role_id     = var.role_id
+#   permissions = var.permissions
+#   org_id      = var.organization
+# }
 
-resource "google_organization_iam_member" "ocean_integration_iam_member" {
-  count  = length(var.projects) == 0 ? 1 : 0
-  org_id = var.organization
-  role   = google_organization_iam_custom_role.ocean_integration_iam_org_role.name
-  member = "serviceAccount:${google_service_account.ocean_integration_service_account.email}"
-}
+# resource "google_organization_iam_member" "ocean_integration_iam_member" {
+#   count  = length(var.projects) == 0 ? 1 : 0
+#   org_id = var.organization
+#   role   = google_organization_iam_custom_role.ocean_integration_iam_org_role.name
+#   member = "serviceAccount:${google_service_account.ocean_integration_service_account.email}"
+# }
 
 
-resource "google_project_iam_binding" "custom_role_binding" {
-  count = length(var.projects)
+# resource "google_project_iam_binding" "custom_role_binding" {
+#   count = length(var.projects)
 
-  project = var.projects[count.index]
-  role    = google_organization_iam_custom_role.ocean_integration_iam_org_role.name
+#   project = var.projects[count.index]
+#   role    = google_organization_iam_custom_role.ocean_integration_iam_org_role.name
 
-  members = [
-    "serviceAccount:${google_service_account.ocean_integration_service_account.email}"
-  ]
-}
+#   members = [
+#     "serviceAccount:${google_service_account.ocean_integration_service_account.email}"
+#   ]
+# }
