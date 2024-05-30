1b972236b6b3213523191a37f7655f687d7f3068
Vladimir Dimov <vladimir.dimov@checkatrade.com>
2024-05-30 09:51:22 +0300

    fix: clearing up

--- modules/aws_helpers/default_events/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "target_arn" {
-  type        = string
-  description = "The ARN of the target"
-}
-
-variable "api_key_param" {
-  type        = string
-  description = "The API Key parameter"
-}
--- modules/azure_helpers/authorization/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "user_assigned_identity_id" {
-  value = azurerm_user_assigned_identity.ocean-azure-assigned-identity.id
-}
-
-output "user_assigned_identity_client_id" {
-  value = azurerm_user_assigned_identity.ocean-azure-assigned-identity.client_id
-}
